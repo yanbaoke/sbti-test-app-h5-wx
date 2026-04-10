15eb77708e2406a332ff2d651ac38d6169aeccd7
hiair2323 <qulili5@yeah.net>
2026-04-10 14:05:36 +0800

    修复 GitHub Pages 资源路径

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
 
 export default defineConfig({
-  plugins: [uni()]
+  plugins: [uni()],
+  base: process.env.NODE_ENV === 'production' ? '/sbti-test-app-h5-wx/' : '/'
 })
